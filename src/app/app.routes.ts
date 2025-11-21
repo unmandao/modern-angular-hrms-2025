c7464ae87a4ab705bc6d270b1daa146d29a23a0e
dbausela <dbausela@gmail.com>
2025-11-21 19:44:47 +0100

    pg. 39

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login-component/login-component';
+import { EmployeeService } from './services/employee-service';
 
 export const routes: Routes = [
     { path: '/login', component: LoginComponent },
@@ -8,9 +9,12 @@ export const routes: Routes = [
             (m) => m.RegistrationComponent
         );
     } },
-    { path: 'employees', loadChildren: () => {
+    { path: 'employees', 
+      providers: [EmployeeService],  
+      loadChildren: () => {
         return import('./pages/employees/employees.routes').then(
             (m) => m.routes,
         );
-    } },
+      }
+    },
 ];
