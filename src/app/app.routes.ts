6efe4ec7942e1c921028c60d1afce9157b9deb89
David Bausela <dbausela@gmail.com>
2025-11-21 07:54:59 +0100

    pg. 35

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,4 +3,9 @@ import { LoginComponent } from './pages/login-component/login-component';
 
 export const routes: Routes = [
     { path: '/login', component: LoginComponent },
+    { path: 'registration', loadComponent: () => {
+        return import('./pages/registration-component/registration-component').then(
+            (m) => m.RegistrationComponent
+        );
+    } },
 ];
