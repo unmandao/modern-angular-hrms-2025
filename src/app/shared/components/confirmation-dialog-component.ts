3d45d400a1d8a986d58f5e7e8893a535e950d190
dbausela <dbausela@gmail.com>
2025-11-24 18:47:39 +0100

    pg. 41

---
+++ src/app/shared/components/confirmation-dialog-component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-confirmation-dialog',
+  template: `
+    <dialog [open]="isConfirmationOpen">
+      Are you sure you want to perform this action?
+      <button (click)="isConfirmationOpen = false">Cancel</button>
+      <button (click)="isConfirmationOpen = false">Confirm</button>
+    </dialog>
+  `,
+})
+export class ConfirmationDialogComponent {
+  @Input() isConfirmationOpen = true;
+  
+}
