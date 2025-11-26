a0df2767e192ecf5015b873f4b1e0ccce908d637
dbausela <dbausela@gmail.com>
2025-11-26 09:44:54 +0100

    pg. 64

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login-component/login-component';
 import { EmployeeService } from './services/employee-service';
+import { authGuard } from './shared/guards/auth.guard';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
@@ -11,6 +12,7 @@ export const routes: Routes = [
     } },
     { path: 'employees', 
       providers: [EmployeeService],  
+      canActivate: [authGuard],
       loadChildren: () => {
         return import('./pages/employees/employees.routes').then(
             (m) => m.routes,
